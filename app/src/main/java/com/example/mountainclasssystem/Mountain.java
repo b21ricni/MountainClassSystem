dee72944c8ad3f2c1c2497b012ef945a0ff571f8
b21ricni <b21ricni@student.his.se>
2022-04-08 14:20:22 +0200

    Seed code

---
+++ app/src/main/java/com/example/mountainclasssystem/Mountain.java
@@ -0,0 +1,14 @@
+package com.example.mountainclasssystem;
+
+public class Mountain {
+    private int height_m;
+    private String name;
+    private String location;
+
+    public Mountain(int height_m, String name, String location) {
+        this.height_m = height_m;
+        this.name = name;
+        this.location = location;
+    }
+}
+
